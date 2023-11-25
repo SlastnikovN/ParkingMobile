6a4fef3f34c66ff584a14689579207d807d4d334
Terok22 <106885591+Terok22@users.noreply.github.com>
2023-11-26 00:17:55 +0500

    Update

--- Screens/Registration.jsx
+++ Screens/Registration.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Button} from 'react-native';
+import {Button, StyleSheet, TextInput} from 'react-native';
 import styled from "styled-components/native";
 import {StatusBar} from "expo-status-bar";
 
@@ -55,12 +55,12 @@ const RegistrationScreen = ({navigation}) => {
             <CarImage source={require('../assets/Image/CarPhotoReg.jpg')}></CarImage>
             <ScreenView_>
                 <RegistrationText>Регистрация</RegistrationText>
-                <TextInp
-                    placeholder="Email"
-                    onChangeText={(text) => setEmail(text)}
-                    value={email}
+                <TextInput style={styles.t}
+                           placeholder="Email"
+                           onChangeText={(text) => setEmail(text)}
+                           value={email}
                 />
-                <TextInp
+                <TextInput style={styles.t}
                     placeholder="Password"
                     secureTextEntry
                     onChangeText={(text) => setPassword(text)}
@@ -72,3 +72,15 @@ const RegistrationScreen = ({navigation}) => {
     );
 };
 export default RegistrationScreen;
+
+const styles = StyleSheet.create({
+    t: {
+        width: 350,
+        height: 40,
+        borderColor: 'gray',
+        borderRadius: 10,
+        borderWidth: 1,
+        marginBottom: 16,
+        paddingHorizontal: 16,
+    }
+});
